317857b33219b4d3576fc4272a94839ba31ef19b
kevinschoenberg <kevin27@outlook.dk>
2024-04-09 19:21:31 +0200

    Added node class with broadcast/n2n functionality

--- src/main.py
+++ src/main.py
@@ -1,15 +1,55 @@
 import simpy
 
-def main():
-    def clock(env, name, tick):
+nodes = []
+
+#TODO Message type class
+
+class Node:
+    def __init__(self, env, name, position, range, isLBR=False):
+        self.env = env
+        self.name = name
+        self.position = position
+        self.range = range
+        self.inbox = []
+        self.isLBR = isLBR
+        self.action = env.process(self.run())
+
+    def run(self):
         while True:
-            print(name, env.now)
-            yield env.timeout(tick)
+            if self.isLBR:
+                self.broadcast('Hello from LBR ' + self.name)
+
+            while self.inbox:
+                message = self.inbox.pop(0)
+                print(self.name + ' received: ' + message)
+
+                #TODO Process message
+
+            yield self.env.timeout(1)
+    
+    def broadcast(self, message):
+        for node in nodes:
+            if node != self and self.distance(node) <= self.range:
+                node.inbox.append(message)
+
+    def send_to(self, node, message):
+        if self.distance(node) <= self.range:
+            node.inbox.append(message)
+
+    def distance(self, node):
+        return ((self.position[0] - node.position[0])**2 + (self.position[1] - node.position[1])**2)**0.5
+
+
+def main():
 
     env = simpy.Environment()
-    env.process(clock(env, 'fast', 0.5))
-    env.process(clock(env, 'slow', 1))
-    env.run(until=2)
+    node1 = Node(env, 'Node1', (0, 0), 4, True)
+    node2 = Node(env, 'Node2', (1, 1), 5)
+    node3 = Node(env, 'Node3', (2, 1), 1)
+    nodes.append(node1)
+    nodes.append(node2)
+    nodes.append(node3)
+    env.run(until=10)
 
 
 if __name__ == "__main__":
