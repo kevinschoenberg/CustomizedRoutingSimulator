2bc4d0b6f32ea20b5125a9c95f1aa1c2715d61de
kevinschoenberg <kevin27@outlook.dk>
2024-04-16 20:37:40 +0200

    Added neighbour discovery

--- src/main.py
+++ src/main.py
@@ -2,11 +2,16 @@ import simpy
 import matplotlib.pyplot as plt
 import random
 
-NUM_NODES = 5 
+NUM_NODES = 40
 AREA_X = 10
 AREA_Y = 10
 
-#TODO Message type class
+
+class Message:
+    def __init__(self, message_type, payload, sender_id):
+        self.message_type = message_type
+        self.payload = payload
+        self.sender_id = sender_id
 
 
 class Connection:
@@ -25,9 +30,10 @@ class Network:
         self.action = env.process(self.run())
     
     def run(self):
-        #global NUM_NODES, AREA_X, AREA_Y
+        # global NUM_NODES, AREA_X, AREA_Y
         self.generate_nodes(self.env, NUM_NODES, AREA_X, AREA_Y)
         self.generate_connections()
+
         while True:
             yield self.env.timeout(1)
 
@@ -48,8 +54,10 @@ class Network:
         plt.figure(figsize=(8, 6))
         for node in self.nodes:
             plt.plot(node.position[0], node.position[1], 'bo')  # Plot node position
-            for neighbor in node.neighbors:
-                plt.plot([node.position[0], neighbor.position[0]], [node.position[1], neighbor.position[1]], 'r-')  # Line between neighbors
+            for neighbor_id in node.neighbors:
+                for node2 in self.nodes:
+                    if node2.node_id == neighbor_id:
+                        plt.plot([node.position[0], node2.position[0]], [node.position[1], node2.position[1]], 'r-')  # Line between neighbors
 
         plt.xlabel('X')
         plt.ylabel('Y')
@@ -62,10 +70,10 @@ class Network:
             isLBR = False
             name = 'Node{}'.format(i + 1)
             position = (random.uniform(0, areaX), random.uniform(0, areaY))  # Random position between (0, 0) and (10, 10)
-            sigRange = 10
+            sigRange = 2
             if i == 0:
                 isLBR = True
-            node = Node(env, name, position, self, sigRange, isLBR)
+            node = Node(env, name, position, self, sigRange, i, isLBR=isLBR)
             self.add_node(node)
 
     def distance(self, node1, node2):
@@ -75,7 +83,7 @@ class Network:
     def generate_connections(self):
         for node1 in self.nodes:
             for node2 in self.nodes:
-                if node1 != node2 and self.distance(node1, node2) <= node1.range:
+                if node1 != node2 and self.in_range(node1, node2):
                     connection = Connection(node1, node2)
                     self.add_connection(connection)
 
@@ -85,10 +93,15 @@ class Network:
             if connection.node1 == node:
                 connection.node2.inbox.append(message)
 
+    def send_message(self, sender, node_id, message):
+        [node.inbox.append(message) for node in self.nodes if node.node_id == node_id and self.in_range(sender, node)]
+
+    def in_range(self, node1, node2):
+        return self.distance(node1, node2) <= node1.range
     
 
 class Node:
-    def __init__(self, env, name, position, network, range, isLBR=False):
+    def __init__(self, env, name, position, network, range, node_id, isLBR=False):
         self.env = env
         self.name = name
         self.position = position
@@ -96,32 +109,40 @@ class Node:
         self.range = range
         self.inbox = []
         self.isLBR = isLBR
+        self.node_id = node_id
         self.neighbors = []
         self.action = env.process(self.run())
-        
 
     def run(self):
-        
+        self.network.broadcast(self, Message("ND", None, self.node_id))
         while True:
-            if self.isLBR:
-                self.network.broadcast(self, 'Hello from LBR ' + self.name)
-            
+            #if self.isLBR:
+                #self.network.broadcast(self, 'Hello from LBR ' + self.name)
 
             while self.inbox:
-                message = self.inbox.pop(0)
+                message: Message = self.inbox.pop(0)
+
+                match message.message_type:
+                    case "ND":
+                        #Received ND message
+                        self.network.send_message(self, message.sender_id, Message("ACK", None, self.node_id))
+                        yield self.env.timeout(0.1)
+                    case "ACK":
+                        self.neighbors.append(message.sender_id)
+                        yield self.env.timeout(0.1)
+                    case "DIO":
+                        pass
+                    case "DAO":
+                        pass
+                    case "DIS":  # Optional
+                        pass
+
+
                 #print(self.name + ' received: ' + message)
 
                 #TODO Process message
 
             yield self.env.timeout(1)
-    
-    def broadcast(self, message):
-        for node in self.network.nodes:
-            if node != self and self.distance(node) <= self.range:
-                node.inbox.append(message)
-    
-    def discover_neighbors(self):
-        self.neighbors = [node for node in self.network.nodes if node != self and self.distance(node) <= self.range]
 
 
 def main():
